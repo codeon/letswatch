a0184fb7db8ef298430f170fb9ff59a859d85336
codeon <mail.kapilgarg@gmail.com>
2013-01-19 12:32:21 +0530

    setup on django

---
+++ letswatch_django/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+from views import *
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns(
+	'',
+    # Examples:
+    url(r'^$', 'letswatch_django.views.home', name='home'),
+    url(r'^letswatch_django/', include('letswatch_django.foo.urls')),
+	url(r'^/home', home),
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    
+)
+
+urlpatterns += staticfiles_urlpatterns()
